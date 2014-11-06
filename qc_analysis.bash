e2cbd947c348962e2ebd9b6a6d86a3a7f852a7ba
Eric Minikel <ericminikel@gmail.com>
2014-11-06 12:58:05 -0500

    DepthOfCoverage

--- qc_analysis.bash
+++ qc_analysis.bash
@@ -1,6 +1,6 @@
 
-
-bsub -q bweek -P $RANDOM -J gtconc -M 8000000 \
+# GenotypeConcordance
+bsub -q week -P $RANDOM -J gtconc -M 8000000 \
             -o gtconc.o \
             -e gtconc.e \
 "java -Xmx8g -jar $gatkjar \
@@ -13,3 +13,32 @@ bsub -q bweek -P $RANDOM -J gtconc -M 8000000 \
               -moltenize \
               -o wgs.vs.wes.gq30dp10.molt"
 
+## DepthOfCoverage
+# WGS
+bsub -q week -P $RANDOM -J doc -M 8000000 \
+    -o docg.o \
+    -e docg.e \
+"java -Xmx8g -jar $gatkjar \
+     -R $b37ref \
+     -T DepthOfCoverage \
+     -o wgs_doc_20_1 \
+     -I $wgs_bams \
+     -L $gencode_cds \
+     --omitDepthOutputAtEachBase \
+     --minBaseQuality 20 \
+     --minMappingQuality 20 \
+     --countType COUNT_FRAGMENTS"
+# WES
+bsub -q week -P $RANDOM -J doc -M 8000000 \
+    -o doce.o \
+    -e doce.e \
+"java -Xmx8g -jar $gatkjar \
+     -R $b37ref \
+     -T DepthOfCoverage \
+     -o wes_doc_20_1 \
+     -I $wes_bams \
+     -L $gencode_cds \
+     --omitDepthOutputAtEachBase \
+     --minBaseQuality 20 \
+     --minMappingQuality 20 \
+     --countType COUNT_FRAGMENTS"

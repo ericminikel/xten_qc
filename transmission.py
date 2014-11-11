2337d7ac596f855784272cafadb9b12749a8dc27
Eric Minikel <ericminikel@gmail.com>
2014-11-10 21:38:29 -0500

    nevermind, don't need a class, can just use a dict if there is only one child per founder

--- transmission.py
+++ transmission.py
@@ -2,19 +2,18 @@
 
 import vcf
 
-class Pedigree:
-    def __init__(self,pedfile):
-        self.d = {}
-        self.founders = []
-        with open(pedfile) as f:
-            for line in f:
-                fid, iid, father, mother, sex, affected = line.strip().split()
-                line_dict = {'fid': fid, 'iid': iid, 'father': father, 'mother': mother, 'sex': sex, 'affected': affected}
-                self.d[iid] = line_dict
-                if father == '.' and mother == '.':
-                    self.founders.append(iid)
-    def display(self):
-        print self.d
-    def is_founder(self,id):
-        return id in self.founders
-
+def read_ped(pedfile):
+    '''
+    Accepts a path to a PED/FAM file. Returns a dictonary with parents as keys and children as values.
+    '''
+    d = {}
+    with open(pedfile) as f:
+        for line in f:
+            print line
+            print len(line.strip().split())
+            fid, iid, father, mother, sex, affected = line.strip().split()
+            if father != '.':
+                d[father] = iid
+            if mother != '.':
+                d[mother] = iid
+    return d

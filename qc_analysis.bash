ffb939f9dc8302a1f28404bb6067da35b3292b8d
Eric Minikel <ericminikel@gmail.com>
2014-11-14 10:44:36 -0500

    do genotype concordance for converse, with WGS as comparison set and WES being evaluated

--- qc_analysis.bash
+++ qc_analysis.bash
@@ -1,4 +1,6 @@
 
+# next time try bsub -R rusage[mem=16G]
+
 # GenotypeConcordance
 bsub -q week -P $RANDOM -J gtconc -M 16000000 \
             -o gtconc.o \
@@ -17,6 +19,24 @@ bsub -q week -P $RANDOM -J gtconc -M 16000000 \
 # extract the one line with the overall summary of concordance for all samples
 cat wgs.vs.wes.gq30dp10.molt | grep -A 1 ^#:GATKTable:GenotypeConcordance_EvalProportions > wgs.vs.wes.gq30dp10.molt.all.concordance.proportions
 cat wgs.vs.wes.gq30dp10.molt | grep -A 400 ^#:GATKTable:GenotypeConcordance_EvalProportions | grep ^ALL >> wgs.vs.wes.gq30dp10.molt.all.concordance.proportions
+
+bsub -q week -P $RANDOM -J gtconc -M 16000000 \
+            -o gtconc.o \
+            -e gtconc.e \
+"java -Xmx15g -jar $gatkjar \
+              -R $b37ref \
+              -T GenotypeConcordance \
+              -gfe 'GQ<30' \
+              -gfe 'DP<10' \
+              -gfc 'GQ<30' \
+              -gfc 'DP<10' \
+              -comp $wgs_vcf \
+              -eval $wes_vcf \
+              -moltenize \
+              -o wes.vs.wgs.gq30dp10.molt"
+cat wes.vs.wgs.gq30dp10.molt | grep -A 1 ^#:GATKTable:GenotypeConcordance_EvalProportions > wes.vs.wgs.gq30dp10.molt.all.concordance.proportions
+cat wes.vs.wgs.gq30dp10.molt | grep -A 400 ^#:GATKTable:GenotypeConcordance_EvalProportions | grep ^ALL >> wes.vs.wgs.gq30dp10.molt.all.concordance.proportions
+
 
 # GenotypeConcordance separately for SNPs and INDELs
 bsub -q week -P $RANDOM -J gtc_snps -M 16000000 \
@@ -77,6 +97,32 @@ bsub -q week -P $RANDOM -J doc -M 24000000 \
      --minBaseQuality 20 \
      --minMappingQuality 20 \
      --countType COUNT_FRAGMENTS"
+
+## DiagnoseTargets instead of DepthOfCoverage
+bsub -q week -P $RANDOM -J doc -M 24000000 \
+    -o docg.o \
+    -e docg.e \
+"java -Xmx23g -jar $gatkjar \
+     -R $b37ref \
+     -T DiagnoseTargets \
+     -BQ 20 \
+     -MQ 20 \
+     -min 10 \
+     -o wgs_diagnosetargets.vcf \
+     -I $wgs_bams \
+     -L $gencode_cds"
+bsub -q week -P $RANDOM -J doc -M 24000000 \
+    -o doce.o \
+    -e doce.e \
+"java -Xmx23g -jar $gatkjar \
+     -R $b37ref \
+     -T DiagnoseTargets \
+     -BQ 20 \
+     -MQ 20 \
+     -min 10 \
+     -o wes_diagnosetargets.vcf \
+     -I $wes_bams \
+     -L $gencode_cds"
 
 
 mkdir jobtemp
